2004e9b60335d7bb1435531f8dda906225fe7004
Anthony Brayan <brayan.farronan@gmail.com>
2023-08-24 11:48:20 +0200

    Add img and Css

--- art-emporium/src/main.jsx
+++ art-emporium/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Headermain from './Components/Header Main/HeadMain';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <>
-
+<Headermain />
 </>
 )
