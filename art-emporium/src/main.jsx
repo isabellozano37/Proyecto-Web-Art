1c8a3a7b2e5ef7f16f934cec4c1a2dd4454b5eff
Anthony Brayan <brayan.farronan@gmail.com>
2023-08-25 16:01:24 +0200

    Button ready, pintura and escultura

--- art-emporium/src/main.jsx
+++ art-emporium/src/main.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Headermain from './Components/Header Main/HeadMain';
+import Headermain from './Components/Header Main/HeadMain.jsx';
+import Arte from './Components/Arte/Arte.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <>
 <Headermain />
+<Arte />
 </>
+
 )
