94206e2154efcb4b448395ac8a5f6d63864dc960
angelserranocorrea <serranocorreaangel@gmail.com>
2023-08-25 10:05:46 +0200

    changes3

--- art-emporium/src/Components/BodyMain/Body.jsx
+++ art-emporium/src/Components/BodyMain/Body.jsx
@@ -1,8 +1,25 @@
 function BodyMain() {
     return(
         <>
-        <h1>Hola</h1>
-        </>
+        <div> 
+        <h1>BIENVENIDOS</h1>
+        </div>
+        
+<div>
+    <div>
+    <h2>Tu puedes amar el arte en todo su esplendor</h2>
+        <h3>Haz click en la imagen</h3>
+    </div>
+    <div>
+        <img src="https://res.cloudinary.com/dhme3c8ll/image/upload/v1692950303/caratula_artes_b8yb9l.jpg"></img>
+    </div>
+
+    
+</div>
+    
+
+    
+       </>
     );
 }
 
--- art-emporium/src/main.jsx
+++ art-emporium/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Headermain from './Components/Header Main/HeadMain';
+import BodyMain from './Components/BodyMain/Body';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <>
