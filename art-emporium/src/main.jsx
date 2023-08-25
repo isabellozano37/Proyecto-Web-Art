48c6320fda69b83f10cd50d4651974552db0834e
isabellozano37 <isabeloficial37@gmail.com>
2023-08-25 12:05:42 +0200

    changes

--- art-emporium/src/main.jsx
+++ art-emporium/src/main.jsx
@@ -1,9 +1,23 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import Headermain from './Components/Header Main/HeadMain';
+import Header from './Components/Header Exponent/HeadExponent';
+import HeaderArt from './Components/Header Art/HeadArt';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ /*ReactDOM.createRoot(document.getElementById('root')).render(
 <>
 <Headermain />
 </>
+)*/
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<>
+<Header />
+</>
 )
+
+/*ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+    <HeaderArt />
+    </>
+)*/
