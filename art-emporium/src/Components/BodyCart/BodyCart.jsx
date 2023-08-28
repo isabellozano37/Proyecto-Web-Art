a120b6b6f8b9752e809f70f11ea97fe4d5eca334
raulmunozrayo <munozrayo.raul@gmail.com>
2023-08-28 12:53:18 +0200

    ayiyiyi

--- art-emporium/src/Components/BodyCart/BodyCart.jsx
+++ art-emporium/src/Components/BodyCart/BodyCart.jsx
@@ -1,46 +1,46 @@
 import "./BodyCart.css";
 
-export default function BodyCart(){
-    return(
+export default function BodyCart() {
+    return (
         <body>
-        <div className="container-card">
-        <div class="card">
-    <figure>
-        <img src="https://mymodernmet.com/wp/wp-content/uploads/2021/05/fine-art-simpsons-fb-thumbnail.jpg"/>
-    </figure>
-    <div class="contenido-card">
-        <h3>BLA BLA BLA</h3>
-        <p>mamawevaso</p>
-        <img id="delete-logo" src="https://res.cloudinary.com/dhme3c8ll/image/upload/v1693216206/1200px-Delete-button.svg_kcuxjd.png"/>
-    </div>
-</div>
-<div class="card">
-    <figure>
-        <img src="https://frinkiac.com/img/S10E19/301750.jpg"/>
-    </figure>
-    <div class="contenido-card">
-        <h3>BLI BLI BLI</h3>
-        <p>mamawevaso</p>
-        <img id="delete-logo" src="https://res.cloudinary.com/dhme3c8ll/image/upload/v1693216206/1200px-Delete-button.svg_kcuxjd.png"/>
-    </div>
-</div>
-<div class="card">
-    <figure>
-        <img src="https://mj2artesanos.es/487-large_default/marco-simpsons.jpg"/>
-    </figure>
-    <div class="contenido-card">
-        <h3>BLI BLI BLI</h3>
-        <p>mamawevaso</p>
-        <img id="delete-logo" src="https://res.cloudinary.com/dhme3c8ll/image/upload/v1693216206/1200px-Delete-button.svg_kcuxjd.png"/>
-    </div>
-    </div>
-</div>
-        <div className="confirmation-container">
-            <p> TOTAL: 999$</p>
-            <button>Comprar</button>
-        </div>
+            <div className="container-card">
+                <div class="card">
+                    <figure>
+                        <img src="https://mymodernmet.com/wp/wp-content/uploads/2021/05/fine-art-simpsons-fb-thumbnail.jpg" />
+                    </figure>
+                    <div class="contenido-card">
+                        <h3>BLA BLA BLA</h3>
+                        <p>mamawevaso</p>
+                        <img id="delete-logo" src="https://res.cloudinary.com/dhme3c8ll/image/upload/v1693216206/1200px-Delete-button.svg_kcuxjd.png" />
+                    </div>
+                </div>
+                <div class="card">
+                    <figure>
+                        <img src="https://frinkiac.com/img/S10E19/301750.jpg" />
+                    </figure>
+                    <div class="contenido-card">
+                        <h3>BLI BLI BLI</h3>
+                        <p>mamawevaso</p>
+                        <img id="delete-logo" src="https://res.cloudinary.com/dhme3c8ll/image/upload/v1693216206/1200px-Delete-button.svg_kcuxjd.png" />
+                    </div>
+                </div>
+                <div class="card">
+                    <figure>
+                        <img src="https://mj2artesanos.es/487-large_default/marco-simpsons.jpg" />
+                    </figure>
+                    <div class="contenido-card">
+                        <h3>BLI BLI BLI</h3>
+                        <p>mamawevaso</p>
+                        <img id="delete-logo" src="https://res.cloudinary.com/dhme3c8ll/image/upload/v1693216206/1200px-Delete-button.svg_kcuxjd.png" />
+                    </div>
+                </div>
+            </div>
+            <div className="confirmation-container">
+                <p> TOTAL: 999$</p>
+                <button>Comprar</button>
+            </div>
         </body>
- ) 
+    )
 
 };
 
