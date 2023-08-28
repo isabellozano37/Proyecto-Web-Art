76fdbeb4aadeffd21b79cc0ef7341e2cced9804a
Anthony Brayan <brayan.farronan@gmail.com>
2023-08-28 12:54:31 +0200

    Headadmin and bodyadmind ok

--- art-emporium/src/main.jsx
+++ art-emporium/src/main.jsx
@@ -4,40 +4,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Headermain from './Components/Header Main/HeadMain.jsx';
 import Arte from './Components/Arte/Arte.jsx';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Headermain from './Components/Header Main/HeadMain';
-import Header from './Components/Header Exponent/HeadExponent';
-import HeaderArt from './Components/Header Art/HeadArt';
-import FormExpo from './Components/Form NewExpo/FormExpo'
-
-
- /*ReactDOM.createRoot(document.getElementById('root')).render(
-<>
-<Headermain />
-<Arte />
-</>
-
-
-=======
-)*/
-
-/*ReactDOM.createRoot(document.getElementById('root')).render(
-<>
-<Header />
-</>
-)*/
-
-/*ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-    <HeaderArt />
-    </>
-)*/
+import Headeradmin from './Components/HeadAdmin/HeadAdmin';
+import BodyAdmin from './Components/BodyAdmin/BodyAdmin';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-    <Headermain />
-
-    <FormExpo />
-    </>
+<>
+{/* <Headermain /> */}
+<Headeradmin />
+{/* <Arte /> */}
+<BodyAdmin />
+</>
 )
+
+
