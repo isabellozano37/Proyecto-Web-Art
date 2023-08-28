b4a8203d56cf6d69886e2380fa211d4c88326456
angelserranocorrea <serranocorreaangel@gmail.com>
2023-08-28 18:34:07 +0200

    ultimos cambios

--- art-emporium/src/main.jsx
+++ art-emporium/src/main.jsx
@@ -13,21 +13,21 @@ import FormBou from './Components/Form NewBou/FormBou.jsx'
 
 
 
-/*ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 <>
 <Headermain />
 
 <BodyMain />
 </>
-)*/
+)
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+/*ReactDOM.createRoot(document.getElementById('root')).render(
     <>
     <HeaderArt />
 
     <Arte />
     </>
-)
+)*/
 
 /*ReactDOM.createRoot(document.getElementById('root')).render(
 <>
