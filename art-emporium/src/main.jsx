16f3690f57f15fd0f81d726dc9c85416173163f9
juanpablolumbipoveda <132656487+juanpablolumbipoveda@users.noreply.github.com>
2023-08-28 17:02:49 +0200

    Changes Footer

--- art-emporium/src/main.jsx
+++ art-emporium/src/main.jsx
@@ -1,9 +1,13 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Headermain from './Components/Header Main/HeadMain';
+import FooterAll from './Components/Footer/FooterAll';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <>
 <Headermain />
+
+<FooterAll />
 </>
 )
