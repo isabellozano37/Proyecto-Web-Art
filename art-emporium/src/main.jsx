e2aab1cf29673ede99c2d9013a55406b594babf8
raulmunozrayo <munozrayo.raul@gmail.com>
2023-08-28 12:38:18 +0200

    create body cart

--- art-emporium/src/main.jsx
+++ art-emporium/src/main.jsx
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom/client'
 import React from 'react'
 import './index.css'
 import HeaderBought from './Components/Header Bought/HeadBought';
+import BodyCart from './Components/BodyCart/BodyCart';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <>
 <HeaderBought />
+<BodyCart/>
 </>
 )
